1dc23e5159bd22d619ad7313f49cd257599330bb
Richard Ricciardelli <ricciardelli2021@gmail.com>
2012-08-19 14:16:46 -0430

    Agregando licencia con formato.

--- i18n/messages.php
+++ i18n/messages.php
@@ -1,9 +1,27 @@
 <?php
-  /*
-  * Este archivo debería tener todos los mensajes de información, confirmación
-  * y de la vista en la aplicación.
-  *
-  */
+/* 
+ * This file is part of ZionPHP.
+ *
+ * ZionPHP is free software you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ZionPHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ZionPHP. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Este archivo debería tener todos los mensajes de información, confirmación
+ * y de la vista en la aplicación.
+ *
+ */
+
   $mpprij = "Ministerio del Poder Popular para Relaciones Interiores y Justicia";
   $license = "<a href='http://www.gnu.org/licenses/gpl.txt'>Licencia GNU GPLv3</a>";
   $footer = "Framework oficial del $mpprij. Software libre bajo $license";
--- i18n/warnings.php
+++ i18n/warnings.php
@@ -1,8 +1,26 @@
 <?php
-  /*
-  * Este archivo debería tener todos los mensajes de advertencia
-  * y confirmación de la aplicación.
-  *
-  */
+/* 
+ * This file is part of ZionPHP.
+ *
+ * ZionPHP is free software you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ZionPHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ZionPHP. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Este archivo debería tener todos los mensajes de advertencia
+ * y confirmación de la aplicación.
+ *
+ */
+
   $foo = "foo";
 ?>
